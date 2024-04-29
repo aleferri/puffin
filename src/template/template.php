8cc7954422c1f7f24b96c6b6901fb0a552610ef6
Alessio Ferri <alessio.ferri.3012@gmail.com>
2024-04-29 17:26:24 +0200

    refactor

--- src/template/template.php
+++ src/template/template.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 /**
  * @author Alessio Ferri
  * @copyright Alessio Ferri
@@ -10,32 +10,32 @@ declare(strict_types=1);
 namespace puffin\template;
 
 function render(string $view, array $args = []): void {
-    if ( $view[0] !== '/' && defined( "TEMPLATES_PATH" ) ) {
+    if ( $view[ 0 ] !== '/' && defined( "TEMPLATES_PATH" ) ) {
         $view = \TEMPLATES_PATH . $view;
     }
     $view = $view . '.php';
 
-    if ( \file_exists ( $view ) ) {
+    if ( \file_exists( $view ) ) {
         include $view;
     } else {
-        throw new \RuntimeException ( 'No such view: ' . $view );
+        throw new \RuntimeException( 'No such view: ' . $view );
     }
 }
 
 function process(string $view, array $args = []): string {
-    if ( $view[0] !== '/' && defined( "TEMPLATES_PATH" ) ) {
+    if ( $view[ 0 ] !== '/' && defined( "TEMPLATES_PATH" ) ) {
         $view = \TEMPLATES_PATH . $view;
     }
     $view = $view . '.php';
 
-    if ( \file_exists ( $view ) ) {
-        \ob_start ();
+    if ( \file_exists( $view ) ) {
+        \ob_start();
         include $view;
-        $contents = \ob_get_contents ();
-        \ob_end_clean ();
+        $contents = \ob_get_contents();
+        \ob_end_clean();
 
         return $contents;
     }
 
-    throw new \RuntimeException ( 'No such view: ' . $view );
+    throw new \RuntimeException( 'No such view: ' . $view );
 }
